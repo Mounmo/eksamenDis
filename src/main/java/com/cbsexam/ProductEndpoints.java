1bfd62ba6be87c7c96ca65ca9944e12eec347b98
Mounya El Rrami <mounyaelrrami@Mounyas-MacBook-Air.local>
2018-11-01 12:52:18 +0100

    Cache has been built and used

--- src/main/java/com/cbsexam/ProductEndpoints.java
+++ src/main/java/com/cbsexam/ProductEndpoints.java
@@ -1,5 +1,6 @@
 package com.cbsexam;
 
+import cache.ProductCache;
 import com.google.gson.Gson;
 import controllers.ProductController;
 import java.util.ArrayList;
@@ -42,13 +43,13 @@ public class ProductEndpoints {
   @Path("/")
   public Response getProducts() {
 
+    ProductCache productCache = new ProductCache();
     // Call our controller-layer in order to get the order from the DB
-    ArrayList<Product> products = ProductController.getProducts();
+    ArrayList<Product> products = productCache.getProducts(false);
 
     // TODO: Add Encryption to JSON: FIX
     // We convert the java object to json with GSON library imported in Maven
     String json = new Gson().toJson(products);
-
 
     json = Encryption.encryptDecryptXOR(json);
 
